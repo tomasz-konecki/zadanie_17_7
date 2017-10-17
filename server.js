b469bb8befe58aad5fee918b7313b94a4ab5de22
Tomasz Konecki <tomasz.konecki.75@gmail.com>
2017-10-18 00:42:05 +0200

    Update code

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const express = require('express'),
     port = 3000;
     app = express();
 
-//set up view engine
 app.set('view engine', 'pug');
 
-//set up routes
 app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
